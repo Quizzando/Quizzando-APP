38a2160b74f6703ad0d2fe6058d3444f756e92bb
Arthur Rocha <145076571+Arthurr-Rosante@users.noreply.github.com>
2025-08-20 08:56:45 -0300

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Quizzando-APP
---
+++ src/routes/__root.tsx
@@ -0,0 +1,33 @@
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanstackDevtools } from '@tanstack/react-devtools'
+
+
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+
+import type { QueryClient } from '@tanstack/react-query'
+import { ThemeProvider } from '@/hooks/useTheme'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>      
+    <Outlet />
+      <TanstackDevtools
+        config={{
+          position: 'bottom-left',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+          TanStackQueryDevtools,
+        ]}
+      />
+    </ThemeProvider>
+  ),
+})
